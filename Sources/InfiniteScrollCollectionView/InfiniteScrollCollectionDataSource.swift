b72746989ec71418fcb73a44014b6654c39e497a
P.Nam <nampp@inspirelab.io>
2022-11-22 11:33:31 +0700

    remove > 0 condition

--- Sources/InfiniteScrollCollectionView/InfiniteScrollCollectionDataSource.swift
+++ Sources/InfiniteScrollCollectionView/InfiniteScrollCollectionDataSource.swift
@@ -23,26 +23,18 @@ extension InfiniteScrollCollectionDataSource: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         guard let dataSource = dataSource else { return 0 }
         let count = dataSource.collectionView(collectionView, numberOfItemsInSection: 0)
-        if count > 0 {
-            let numberOfSets = dataSource.numberOfSets(in: collectionView)
-            return count + (2 * numberOfSets)
-        } else {
-            return 0
-        }
+        let numberOfSets = dataSource.numberOfSets(in: collectionView)
+        return count + (2 * numberOfSets)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let dataSource = dataSource else { return UICollectionViewCell(frame: .zero) }
         let realCount = dataSource.collectionView(collectionView, numberOfItemsInSection: 0)
-        if realCount > 0 {
-            let numberOfSets = dataSource.numberOfSets(in: collectionView)
-            var calculateIndex = (indexPath.row - numberOfSets) % realCount
-            if calculateIndex < 0  {
-                calculateIndex = realCount - abs(calculateIndex)
-            }
-            return dataSource.collectionView(collectionView, cellForItemAt: IndexPath(item: calculateIndex, section: 0))
-        } else {
-            return dataSource.collectionView(collectionView, cellForItemAt: indexPath)
+        let numberOfSets = dataSource.numberOfSets(in: collectionView)
+        var calculateIndex = (indexPath.row - numberOfSets) % realCount
+        if calculateIndex < 0  {
+            calculateIndex = realCount - abs(calculateIndex)
         }
+        return dataSource.collectionView(collectionView, cellForItemAt: [0, calculateIndex])
     }
 }
